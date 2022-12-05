4290a50ca0442c3c2a4230e4cdf78d517c7dd241
Moritz <fullermoritz@gmail.com>
2022-12-05 18:59:09 +0100

    🚧

--- src/components/BuyNftModal.svelte
+++ src/components/BuyNftModal.svelte
@@ -2,26 +2,30 @@
   import Button from "./Button.svelte";
   import Modal from "./Modal.svelte";
   import { store } from "../store";
-  import { createEventDispatcher } from 'svelte';
+  import spinner from "../assets/loading.gif";
+  import { createEventDispatcher } from "svelte";
   import Loader from "./Loader.svelte";
+  import { fromErr, fromOk, isErr } from "../utils";
 
   export let toggleBuyModal;
   export let count: bigint;
   export let price: bigint;
 
-  let step: 'confirm' | 'buying' | 'bought' | 'error' = 'confirm';
-  let progressText = '';
-  let errorText = '';
+  let loading = false;
+  let error = "";
+  let step: "confirm" | "buying" | "bought" | "error" = "confirm";
+  let progressText = "";
+  let errorText = "";
   let dispatch = createEventDispatcher();
 
-  $: {
-    if (!open) {
-      reset();
-    }
-  };
+  // $: {
+  //   if (!open) {
+  //     reset();
+  //   }
+  // }
 
-//   the method signature of `reserver` asks for a subaccount. 
-// that parameter isn't used though so we just pass random bytes
+  //   the method signature of `reserver` asks for a subaccount.
+  // that parameter isn't used though so we just pass random bytes
   const _getRandomBytes = () => {
     var bs = [];
     for (var i = 0; i < 32; i++) {
@@ -31,38 +35,39 @@
   };
 
   function reset() {
-    step = 'confirm';
-    progressText = '';
-    errorText = '';
-  }
-
-  async function close() {
-    // open = false;
+    step = "confirm";
+    progressText = "";
+    errorText = "";
   }
 
   async function buy() {
-    step = 'buying';
-    progressText = 'Reserving NFT...';
+    step = "buying";
+    progressText = "Reserving NFT...";
 
     try {
       // reserve
       let accountId = $store.accountId;
-      console.log('reserving for account', accountId);
-      let res = await $store.extActor.reserve(price, count, accountId, _getRandomBytes());
+      console.log("reserving for account", accountId);
+      let res = await $store.extActor.reserve(
+        price,
+        count,
+        accountId,
+        _getRandomBytes()
+      );
 
-      if ('err' in res) {
-        throw res.err;
+      if (isErr(res)) {
+        throw fromErr(res);
       }
 
-      var payToAddress = res.ok[0];
-      var priceToPay = res.ok[1];
+      let payToAddress = fromOk(res)[0];
+      let priceToPay = fromOk(res)[1];
 
       // transfer ICP
-      progressText = 'Transferring ICP...';
+      progressText = "Transferring ICP...";
       await store.transfer(payToAddress, priceToPay);
 
       // retreive
-      progressText = 'Completing purchase...';
+      progressText = "Completing purchase...";
 
       while (true) {
         let res;
@@ -71,16 +76,16 @@
         } catch (e) {
           continue;
         }
-        if ('ok' in res) break;
-        if ('err' in res)
-          throw 'Your purchase failed! If ICP was sent and the sale ran out, you will be refunded shortly!';
+        if ("ok" in res) break;
+        if ("err" in res)
+          throw "Your purchase failed! If ICP was sent and the sale ran out, you will be refunded shortly!";
       }
-      
-      step = 'bought';
 
-      dispatch('success');
+      step = "bought";
+
+      dispatch("success");
     } catch (err) {
-      step = 'error';
+      step = "error";
       errorText = err;
       throw err;
     }
@@ -88,37 +93,59 @@
 </script>
 
 <Modal title="Buy NFT" toggleModal={toggleBuyModal}>
-  {#if step == 'confirm'}
-    <div class="flex flex-col gap-5">
-      <div class="text-xl text-left px-6 py-12 my-6">
-        Are you sure you want to continue with this purchase of <b>{count}</b> NFT{count === 1n ? '' : 's'} for the total price of <b>{BigInt(price) / 100000000n}</b> ICP?
-        All transactions are final on confirmation and can't be reversed.
-      </div>
-      <div class="flex gap-3 justify-end">
-        <Button on:click={buy}>BUY</Button>
-        <Button on:click={close}>Cancel</Button>
-      </div>
+  {#if step == "confirm"}
+    <div class="mt-2 dark:text-white lg:text-3xl 2xl:text-4xl">
+      Are you sure you want to continue with this purchase of <b>{count}</b>
+      NFT{count === 1n ? "" : "s"} for the total price of
+      <b>{BigInt(price) / 100000000n}</b> ICP? All transactions are final on confirmation
+      and can't be reversed.
     </div>
-  {:else if step == 'buying'}
+    <div class="flex gap-3 flex-col flex-1 justify-center items-center">
+      <Button
+        on:click={buy}
+        disabled={loading}
+        style={"lg:h-16 2xl:h-20 lg:rounded-[55px] disabled:cursor-not-allowed"}
+      >
+        {#if loading}
+          <img class="h-6" src={spinner} alt="loading animation" />
+        {:else}
+          submit
+        {/if}
+      </Button>
+      <Button
+        on:click={toggleBuyModal}
+        style={"lg:h-16 2xl:h-20 lg:rounded-[55px]"}>cancel</Button
+      >
+    </div>
+  {:else if step == "buying"}
     <div class="flex items-center justify-center gap-5 my-20">
-      <Loader></Loader>
+      <Loader />
       <div class="text-3xl">{progressText}</div>
     </div>
-  {:else if step == 'bought'}
+  {:else if step == "bought"}
     <div class="flex flex-col justify-center gap-5 my-8">
-      <div class="text-3xl text-green-800">Success!</div>
-      <div class="text-2xl">Your purchase was made successfully - your NFT will be sent to your address shortly!</div>
+      <div class="mt-2 dark:text-white lg:text-3xl 2xl:text-4xl">
+        Your purchase was made successfully - your NFT will be sent to your
+        address shortly!
+      </div>
+      <div class="flex gap-3 flex-col flex-1 justify-center items-center">
+        <Button
+          on:click={toggleBuyModal}
+          style={"lg:h-16 2xl:h-20 lg:rounded-[55px]"}>close</Button
+        >
+      </div>
     </div>
-    <div class="flex gap-3 justify-end">
-      <Button on:click={close}>Close</Button>
-    </div>
-  {:else if step == 'error'}
-    <div class="flex flex-col gap-5 my-8 text-left">
-      <div class="text-3xl text-red-800">Error :(</div>
-      <div class="text-2xl">{errorText}</div>
-    </div>
-    <div class="flex gap-3 justify-end">
-      <Button on:click={close}>Close</Button>
+  {:else if step == "error"}
+    <div class="flex flex-col justify-center gap-5 my-8">
+      <div class="mt-2 dark:text-white lg:text-3xl 2xl:text-4xl">
+        {errorText}
+      </div>
+      <div class="flex gap-3 flex-col flex-1 justify-center items-center">
+        <Button
+          on:click={toggleBuyModal}
+          style={"lg:h-16 2xl:h-20 lg:rounded-[55px]"}>close</Button
+        >
+      </div>
     </div>
   {/if}
 </Modal>
