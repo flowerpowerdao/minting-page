d712d85e06ad7054765f89b6477d6e30d0da1cc1
Moritz <fullermoritz@gmail.com>
2022-12-09 14:01:33 +0100

    🎨 formatting

--- src/store.ts
+++ src/store.ts
@@ -4,15 +4,15 @@ import type { HttpAgent, Identity } from "@dfinity/agent";
 import { StoicIdentity } from "ic-stoic-identity";
 import { AccountIdentifier, SubAccount } from "@dfinity/nns";
 import {
-    ext,
-    createActor as createExtActor,
-    idlFactory as extIdlFactory,
+  ext,
+  createActor as createExtActor,
+  idlFactory as extIdlFactory,
 } from "./declarations/ext";
 import {
-    ledger,
-    createActor as createLedgerActor,
-    idlFactory as ledgerIdlFactory,
-    canisterId as ledgerCanisterId,
+  ledger,
+  createActor as createLedgerActor,
+  idlFactory as ledgerIdlFactory,
+  canisterId as ledgerCanisterId,
 } from "./declarations/ledger";
 
 // we can't use the canister id from the ext declarations, as
@@ -21,287 +21,285 @@ import {
 import { collection } from "./collection";
 
 export const HOST =
-    process.env.DFX_NETWORK !== "ic"
-        ? "http://localhost:4943"
-        : "https://ic0.app";
+  process.env.DFX_NETWORK !== "ic"
+    ? "http://localhost:4943"
+    : "https://ic0.app";
 
 type State = {
-    isAuthed: "plug" | "stoic" | null;
-    extActor: typeof ext;
-    ledgerActor: typeof ledger;
-    principal: Principal;
-    accountId: string;
-    error: string;
-    isLoading: boolean;
-    isBuying: boolean;
+  isAuthed: "plug" | "stoic" | null;
+  extActor: typeof ext;
+  ledgerActor: typeof ledger;
+  principal: Principal;
+  accountId: string;
+  error: string;
+  isLoading: boolean;
+  isBuying: boolean;
 };
 
 const defaultState: State = {
-    isAuthed: null,
-    extActor: createExtActor(collection.canisterId, {
-        agentOptions: { host: HOST },
-    }),
-    ledgerActor: createLedgerActor(ledgerCanisterId, {
-        agentOptions: { host: HOST },
-    }),
-    principal: null,
-    accountId: "",
-    error: "",
-    isLoading: false,
-    isBuying: false,
+  isAuthed: null,
+  extActor: createExtActor(collection.canisterId, {
+    agentOptions: { host: HOST },
+  }),
+  ledgerActor: createLedgerActor(ledgerCanisterId, {
+    agentOptions: { host: HOST },
+  }),
+  principal: null,
+  accountId: "",
+  error: "",
+  isLoading: false,
+  isBuying: false,
 };
 
 export const createStore = ({
-    whitelist,
-    host,
+  whitelist,
+  host,
 }: {
-    whitelist?: string[];
-    host?: string;
+  whitelist?: string[];
+  host?: string;
 }) => {
-    const { subscribe, update } = writable<State>(defaultState);
+  const { subscribe, update } = writable<State>(defaultState);
 
-    const stoicConnect = () => {
-        StoicIdentity.load().then(async (identity) => {
-            if (identity !== false) {
-                // ID is a already connected wallet!
-            } else {
-                // No existing connection, lets make one!
-                identity = await StoicIdentity.connect();
-            }
-            initStoic(identity);
-        });
-    };
+  const stoicConnect = () => {
+    StoicIdentity.load().then(async (identity) => {
+      if (identity !== false) {
+        // ID is a already connected wallet!
+      } else {
+        // No existing connection, lets make one!
+        identity = await StoicIdentity.connect();
+      }
+      initStoic(identity);
+    });
+  };
 
-    const initStoic = async (identity: Identity & { accounts(): string }) => {
-        console.trace("initStoic");
+  const initStoic = async (identity: Identity & { accounts(): string }) => {
+    console.trace("initStoic");
 
-        const extActor = createExtActor(collection.canisterId, {
-            agentOptions: {
-                identity,
-                host: HOST,
-            },
-        });
+    const extActor = createExtActor(collection.canisterId, {
+      agentOptions: {
+        identity,
+        host: HOST,
+      },
+    });
 
-        const ledgerActor = createLedgerActor(ledgerCanisterId, {
-            agentOptions: {
-                identity,
-                host: HOST,
-            },
-        });
+    const ledgerActor = createLedgerActor(ledgerCanisterId, {
+      agentOptions: {
+        identity,
+        host: HOST,
+      },
+    });
 
-        if (!extActor || !ledgerActor) {
-            console.warn("couldn't create actors");
-            return;
-        }
-
-        // the stoic agent provides an `accounts()` method that returns
-        // accounts assocaited with the principal
-        let accounts = JSON.parse(await identity.accounts());
-
-        update((state) => ({
-            ...state,
-            principal: identity.getPrincipal(),
-            accountId: accounts[0].address, // we take the default account associated with the identity
-        }));
-
-        update((state) => ({
-            ...state,
-            extActor,
-            ledgerActor,
-            principal: identity.getPrincipal(),
-            isAuthed: "stoic",
-        }));
-    };
-
-    const plugConnect = async () => {
-        // check if plug is installed in the browser
-        if (window.ic?.plug === undefined) {
-            window.open("https://plugwallet.ooo/", "_blank");
-            return;
-        }
-
-        // check if plug is connected
-        const plugConnected = await window.ic?.plug?.isConnected();
-        if (!plugConnected) {
-            try {
-                console.log({
-                    whitelist,
-                    host,
-                });
-                await window.ic?.plug.requestConnect({
-                    whitelist,
-                    host,
-                });
-                console.log("plug connected");
-            } catch (e) {
-                console.warn(e);
-                return;
-            }
-        }
-
-        await initPlug();
-    };
-
-    const initPlug = async () => {
-        // check wether agent is present
-        // if not create it
-        if (!window.ic?.plug?.agent) {
-            console.warn("no agent found");
-            const result = await window.ic?.plug?.createAgent({
-                whitelist,
-                host,
-            });
-            result
-                ? console.log("agent created")
-                : console.warn("agent creation failed");
-        }
-        // check of if createActor method is available
-        if (!window.ic?.plug?.createActor) {
-            console.warn("no createActor found");
-            return;
-        }
-
-        // Fetch root key for certificate validation during development
-        if (process.env.DFX_NETWORK !== "ic") {
-            window.ic.plug.agent.fetchRootKey().catch((err) => {
-                console.warn(
-                    "Unable to fetch root key. Check to ensure that your local replica is running"
-                );
-                console.error(err);
-            });
-        }
-
-        const extActor = (await window.ic?.plug.createActor({
-            canisterId: collection.canisterId,
-            interfaceFactory: extIdlFactory,
-        })) as typeof ext;
-
-        if (!extActor) {
-            console.warn("couldn't create actors");
-            return;
-        }
-
-        const principal = await window.ic.plug.agent.getPrincipal();
-        update((state) => ({
-            ...state,
-            extActor,
-            principal,
-            accountId: window.ic.plug.sessionManager.sessionData.accountId,
-            isAuthed: "plug",
-        }));
-
-        console.log("plug is authed");
-    };
-
-    async function transfer(toAddress: string, amount: bigint) {
-        const store = get({ subscribe });
-
-        if (store.isAuthed === "plug") {
-            let height = await window.ic.plug.requestTransfer({
-                to: toAddress,
-                amount: Number(amount),
-                opts: {
-                  fee: 10000
-                }
-            });
-            console.log("sent", height);
-        } else if (store.isAuthed === "stoic") {
-            console.log("send_dfx...");
-            let res = await store.ledgerActor.transfer({
-                from_subaccount: [],
-                to: AccountIdentifier.fromHex(toAddress).toNumbers(),
-                amount: { e8s: amount },
-                fee: { e8s: 10000n },
-                memo: 0n,
-                created_at_time: [],
-            });
-            console.log("sent", res);
-        }
+    if (!extActor || !ledgerActor) {
+      console.warn("couldn't create actors");
+      return;
     }
 
-    const disconnect = async () => {
-        console.log("disconnected");
-        StoicIdentity.disconnect();
-        window.ic?.plug?.disconnect();
-        // wait for 500ms to ensure that the disconnection is complete
-        await new Promise((resolve) => setTimeout(resolve, 500));
-        console.log("plug status: ", await window.ic?.plug?.isConnected());
+    // the stoic agent provides an `accounts()` method that returns
+    // accounts assocaited with the principal
+    let accounts = JSON.parse(await identity.accounts());
 
-        update((prevState) => {
-            return {
-                ...defaultState,
-            };
+    update((state) => ({
+      ...state,
+      principal: identity.getPrincipal(),
+      accountId: accounts[0].address, // we take the default account associated with the identity
+    }));
+
+    update((state) => ({
+      ...state,
+      extActor,
+      ledgerActor,
+      principal: identity.getPrincipal(),
+      isAuthed: "stoic",
+    }));
+  };
+
+  const plugConnect = async () => {
+    // check if plug is installed in the browser
+    if (window.ic?.plug === undefined) {
+      window.open("https://plugwallet.ooo/", "_blank");
+      return;
+    }
+
+    // check if plug is connected
+    const plugConnected = await window.ic?.plug?.isConnected();
+    if (!plugConnected) {
+      try {
+        console.log({
+          whitelist,
+          host,
         });
-    };
+        await window.ic?.plug.requestConnect({
+          whitelist,
+          host,
+        });
+        console.log("plug connected");
+      } catch (e) {
+        console.warn(e);
+        return;
+      }
+    }
 
-    return {
-        subscribe,
-        update,
-        plugConnect,
-        stoicConnect,
-        disconnect,
-        transfer,
-    };
+    await initPlug();
+  };
+
+  const initPlug = async () => {
+    // check wether agent is present
+    // if not create it
+    if (!window.ic?.plug?.agent) {
+      console.warn("no agent found");
+      const result = await window.ic?.plug?.createAgent({
+        whitelist,
+        host,
+      });
+      result
+        ? console.log("agent created")
+        : console.warn("agent creation failed");
+    }
+    // check of if createActor method is available
+    if (!window.ic?.plug?.createActor) {
+      console.warn("no createActor found");
+      return;
+    }
+
+    // Fetch root key for certificate validation during development
+    if (process.env.DFX_NETWORK !== "ic") {
+      window.ic.plug.agent.fetchRootKey().catch((err) => {
+        console.warn(
+          "Unable to fetch root key. Check to ensure that your local replica is running"
+        );
+        console.error(err);
+      });
+    }
+
+    const extActor = (await window.ic?.plug.createActor({
+      canisterId: collection.canisterId,
+      interfaceFactory: extIdlFactory,
+    })) as typeof ext;
+
+    if (!extActor) {
+      console.warn("couldn't create actors");
+      return;
+    }
+
+    const principal = await window.ic.plug.agent.getPrincipal();
+    update((state) => ({
+      ...state,
+      extActor,
+      principal,
+      accountId: window.ic.plug.sessionManager.sessionData.accountId,
+      isAuthed: "plug",
+    }));
+
+    console.log("plug is authed");
+  };
+
+  async function transfer(toAddress: string, amount: bigint) {
+    const store = get({ subscribe });
+
+    if (store.isAuthed === "plug") {
+      let height = await window.ic.plug.requestTransfer({
+        to: toAddress,
+        amount: Number(amount),
+        opts: {
+          fee: 10000,
+        },
+      });
+      console.log("sent", height);
+    } else if (store.isAuthed === "stoic") {
+      console.log("send_dfx...");
+      let res = await store.ledgerActor.transfer({
+        from_subaccount: [],
+        to: AccountIdentifier.fromHex(toAddress).toNumbers(),
+        amount: { e8s: amount },
+        fee: { e8s: 10000n },
+        memo: 0n,
+        created_at_time: [],
+      });
+      console.log("sent", res);
+    }
+  }
+
+  const disconnect = async () => {
+    console.log("disconnected");
+    StoicIdentity.disconnect();
+    window.ic?.plug?.disconnect();
+    // wait for 500ms to ensure that the disconnection is complete
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    console.log("plug status: ", await window.ic?.plug?.isConnected());
+
+    update((prevState) => {
+      return {
+        ...defaultState,
+      };
+    });
+  };
+
+  return {
+    subscribe,
+    update,
+    plugConnect,
+    stoicConnect,
+    disconnect,
+    transfer,
+  };
 };
 
 export const store = createStore({
-    whitelist: [
-        collection.canisterId,
-        // 'ryjl3-tyaaa-aaaaa-aaaba-cai',
-    ],
-    host: HOST,
+  whitelist: [
+    collection.canisterId,
+    // 'ryjl3-tyaaa-aaaaa-aaaba-cai',
+  ],
+  host: HOST,
 });
 
 declare global {
-    interface Window {
-        ic: {
-            plug: {
-                agent: HttpAgent;
-                sessionManager: {
-                    sessionData: {
-                        accountId: string;
-                    };
-                };
-                getPrincipal: () => Promise<Principal>;
-                deleteAgent: () => void;
-                requestConnect: (options?: {
-                    whitelist?: string[];
-                    host?: string;
-                }) => Promise<any>;
-                createActor: (options: {}) => Promise<
-                    typeof ledger | typeof ext
-                >;
-                isConnected: () => Promise<boolean>;
-                disconnect: () => Promise<boolean>;
-                createAgent: (args?: {
-                    whitelist: string[];
-                    host?: string;
-                }) => Promise<undefined>;
-                requestBalance: () => Promise<
-                    Array<{
-                        amount: number;
-                        canisterId: string | null;
-                        image: string;
-                        name: string;
-                        symbol: string;
-                        value: number | null;
-                    }>
-                >;
-                requestTransfer: (arg: {
-                  to: string;
-                  amount: number;
-                  opts?: {
-                    fee?: number;
-                    memo?: string;
-                    from_subaccount?: number;
-                    created_at_time?: {
-                      timestamp_nanos: number;
-                    };
-                  };
-                }) => Promise<{ height: number }>;
-            };
+  interface Window {
+    ic: {
+      plug: {
+        agent: HttpAgent;
+        sessionManager: {
+          sessionData: {
+            accountId: string;
+          };
         };
-    }
+        getPrincipal: () => Promise<Principal>;
+        deleteAgent: () => void;
+        requestConnect: (options?: {
+          whitelist?: string[];
+          host?: string;
+        }) => Promise<any>;
+        createActor: (options: {}) => Promise<typeof ledger | typeof ext>;
+        isConnected: () => Promise<boolean>;
+        disconnect: () => Promise<boolean>;
+        createAgent: (args?: {
+          whitelist: string[];
+          host?: string;
+        }) => Promise<undefined>;
+        requestBalance: () => Promise<
+          Array<{
+            amount: number;
+            canisterId: string | null;
+            image: string;
+            name: string;
+            symbol: string;
+            value: number | null;
+          }>
+        >;
+        requestTransfer: (arg: {
+          to: string;
+          amount: number;
+          opts?: {
+            fee?: number;
+            memo?: string;
+            from_subaccount?: number;
+            created_at_time?: {
+              timestamp_nanos: number;
+            };
+          };
+        }) => Promise<{ height: number }>;
+      };
+    };
+  }
 }
 
 // window.store = store;
