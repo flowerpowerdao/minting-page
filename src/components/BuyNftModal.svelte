bf83895bf364260d0784528f6b16b3796438fb41
Zen Voich <zen.voich@gmail.com>
2023-01-03 17:06:49 +0400

    display preview of NFT minted

--- src/components/BuyNftModal.svelte
+++ src/components/BuyNftModal.svelte
@@ -6,15 +6,19 @@
   import spinner from "../assets/loading.gif";
   import Loader from "./Loader.svelte";
   import { fromErr, fromOk, isErr } from "../utils";
+  import { collection } from "../collection";
 
   export let toggleBuyModal;
   export let count: bigint;
   export let price: bigint;
 
+  let isDev = process.env.NODE_ENV;
   let loading = false;
   let step: "confirm" | "buying" | "bought" | "error" = "confirm";
   let progressText = "";
   let errorText = "";
+  let boughtTokenIndex: number;
+
   $: console.log(step);
 
   //   the method signature of `reserver` asks for a subaccount.
@@ -82,10 +86,17 @@
           console.log(`bought nft ${fromOk(res)}`);
           break;
         }
-        if ("err" in res)
+        if ("err" in res) {
           throw "Your purchase failed! If ICP was sent and the sale ran out, you will be refunded shortly!"; // this throw will be caught at the end of the method
+        };
       }
 
+      // get just bought token
+      let tokens = await $store.extActor.tokens(accountId);
+      if ('ok' in tokens) {
+        boughtTokenIndex = tokens.ok.at(-1);
+      }
+      
       step = "bought";
 
       store.update((state) => ({
@@ -147,6 +158,15 @@
     <div class="dark:text-white lg:text-3xl 2xl:text-4xl">
       Your purchase was made successfully - your NFT will be sent to your
       address shortly!
+    </div>
+    <div class="flex justify-center">
+      {#if isDev === 'development'}
+        <!-- <img src="https://4ggk4-mqaaa-aaaae-qad6q-cai.raw.ic0.app/{boughtTokenIndex}" alt=""> -->
+        <!-- <img src="https://4bhmi-biaaa-aaaae-qad6a-cai.raw.ic0.app/537.svg" alt=""> -->
+        <img src="/src/assets/svelte.png" alt="">
+      {:else}
+        <img src="https://{collection.canisterId}.raw.ic0.app/{boughtTokenIndex}" alt="">
+      {/if}
     </div>
     <div class="flex gap-3 flex-col flex-1 justify-center items-center mt-6">
       <Button
