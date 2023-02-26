54408e4f8e5a7089f8acc637b1345ed0f0b77312
Moritz <fullermoritz@gmail.com>
2023-02-26 16:21:03 +0100

    ✨ updated to production

--- src/collection.ts
+++ src/collection.ts
@@ -5,7 +5,7 @@ export let collection = {
   canisterId:
     process.env.NODE_ENV === "development"
       ? localCanisterIds.staging.local
-      : "2s47y-zyaaa-aaaae-qaewq-cai",
+      : "2v5zm-uaaaa-aaaae-qaewa-cai",
   description:
     "In a stunning display of artistic expression, the renowned creator Ludo has taken the reins of Clown Skateboards' infamous logo, which was first crafted by the illustrious Banksy. With four mesmerizing refixes, Ludo deftly employs a palette of colors drawn from our collective memory of the most unforgettable figures and events of the past two decades, imbuing this latest release with a distinctive character and elevating Clown Skateboards into the upper echelon of the Internet Computer.",
   // show nft asset preview immediately after purchase
